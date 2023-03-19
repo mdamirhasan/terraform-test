35bb5a35645e92025624a940ea6c1dfad692f7ed
mdamirhasan <Amir.Hasan@shorthillstech.com>
2023-03-19 14:20:18 +0530

    update main.tf file

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-resource "aws_instance" "Example" {
-  ami           =  "ami-08795883c7b4b7140" 
+resource "aws_instance" "example" {
+  ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-example"
+  }
 }
